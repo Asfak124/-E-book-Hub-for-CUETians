2021ca8b4a72b18eec97da9e2a13aa8fdf716852
u1704127 <forfammail@gmail.com>
2023-02-03 22:54:27 +0600

    eBookHub

---
+++ resources/views/frontend/layouts/partials/footer.blade.php
@@ -0,0 +1,40 @@
+
+<!-- Footer Start -->
+<div class="footer-area">
+  <div class="container">
+    <div class="row">
+      
+      <div class="col-md-9">
+        <img src="{{ asset('images/logo.jpg') }}" class="logo-image">
+        <p>
+          <address>
+            Bangabandhu Hall, CUET
+            <br>
+            Chittagonh
+          </address>
+          <a href="mailto:u1704124@student.cuet.ac.bd">u1704124@student.cuet.ac.bd</a>
+        </p>
+      </div>
+
+      <div class="col-md-1">
+        
+      
+      </div>
+
+
+      <div class="col-md-1">
+         
+      </div>
+
+
+      <div class="col-md-1">
+       
+      </div>
+
+    </div>
+
+    <p class="text-center">
+      &copy;  All rights reserved to 1704075-1704124-1704127
+    </p>
+  </div>
+</div>
